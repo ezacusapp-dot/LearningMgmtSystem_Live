8941016870fa256f67c779d89fe1242988cde4e7
ezacusapp-dot <ezacusapp@gmail.com>
2026-04-13 16:22:33 +0530

    update

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -60,7 +60,7 @@ export default function SignInPage() {
             setLoading(true);
 console.log(process.env.NEXT_PUBLIC_API_URL);
             const res = await fetch(
-                `${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`,
+                `/api/auth/login`,
                 {
                     method: "POST",
                     headers: {
