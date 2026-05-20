47493025b73c9a7c2a09f39c9de2391ab12bfae0
ezacusapp-dot <ezacusapp@gmail.com>
2026-05-20 16:50:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,41 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["paseto"],
-  allowedDevOrigins: ["*"], // ✅ allow all origins
+
+  allowedDevOrigins: ["*"],
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              "https://learning-mgmt-system-alpha.vercel.app",
+          },
+
+          {
+            key: "Access-Control-Allow-Methods",
+            value:
+              "GET, POST, PUT, DELETE, OPTIONS",
+          },
+
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Content-Type, Authorization",
+          },
+
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
